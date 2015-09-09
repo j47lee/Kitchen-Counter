7d431e2437fd8bc17c1cf22264e23c1ef990c385
Jonathan Lee <j47lee@gmail.com>
2015-09-09 15:53:27 -0700

    logic testing

--- TEST_CODE_ONLY/query.js
+++ TEST_CODE_ONLY/query.js
@@ -9,12 +9,12 @@ allRecipes = [
       "imageUrl": "http://images.media-allrecipes.com/userphotos/720x405/630443.jpg",
       "ingredients": [
           {
-              "name": "mango",
+              "name": "angel hair pasta",
               "qty": "10",
               "unit": "grams"
           },
           {
-              "name": "angel hair pasta",
+              "name": "mango",
               "qty": "15",
               "unit": "grams"
           }
@@ -50,6 +50,7 @@ allRecipes = [
 
 ];
 
+//MY INGREDIENTS ================================================================
 myObject = {
     "_id": {
         "$oid": "55ee814fa694923017a3f2ed"
@@ -98,66 +99,101 @@ myObject = {
 }
 
 // ------------------------------------------------------
-//QUERY MY USER OBJECT AND CREATING ARRAY OF INGREDIENTS =======================
+//CREATING ARRAY OF MY INGREDIENTS FROM USER OBJECT ====================
 var myIngredientsArray = [];
 myIngredients = myObject.ingredients
 for (var k = 0; k < myIngredients.length; k++) {
   myIngredientsArray.push(myIngredients[k].name)
 }
-console.log('My array of ingredients', myIngredientsArray);
+// console.log(myIngredientsArray);
+
+//COMPARE FUNCTION =============================================================
+function check(arr) {
+    var totalmatches = 0;
+    for (i = 0; i < myIngredientsArray.length; i++) {
+        for (j = 0; j < arr.length; j++) {
+
+            if (myIngredientsArray[i] == arr[j]) {
+
+                // console.log('MATCHES', myIngredientsArray[i], arr[j]);
+                totalmatches++;
+
+            } else {
+                // console.log('NO MATCH', myIngredientsArray[i], arr[j]);
+            } //end if-statement
+
+        } //end for-loop on 'arr' (which is an array of each recipe's ingredients)
+    } //end for-loop on myIngredientsArray
+
+    if (totalmatches/arr.length === 1) {
+        console.log('Recipe matches 100%');
+    } else if (totalmatches/arr.length >= 0.8) {
+        console.log("100% >= Recipe matches your Ingredients >= 80%");
+    } else if (totalmatches/arr.length >= 0.5) {
+        console.log("80% >= Recipe matches your Ingredients >= 50%");
+    } else {
+        console.log('No recipes match your ingredients');
+    }
+}
 
 //QUERY ALL RECIPES =============================================================
 var ingredientsArray = [];
-//grabbing one recipe
+//grabbing each recipe
 for (var i = 0; i < allRecipes.length; i++) {
     eachRecipeIngredients = allRecipes[i].ingredients
     // console.log(eachRecipeIngredients);
-  for (var j = 0; j < eachRecipeIngredients.length; j++) {
-    eachIngredient = eachRecipeIngredients[j].name
-    // console.log(eachRecipeIngredients[j].name);
-    ingredientsArray.push(eachIngredient)
-  }
-
-
-  ///////// COMPARE MY INGREDIENTS WITH EACH RECIPE HERE ////////////////
-
-
-
-  console.log('Recipe index', i, ingredientsArray);
-  ingredientsArray = [];
-  // console.log(ingredientsArray);
-}
-
-//LOGIC ============================================================
-mine = ['pasta','banana','fish','cheese','water','red salsa','duck','chicken']
-
-function check(arr) {
-    var totalmatches = 0;
-    for (i = 0; i < mine.length; i++) {
-        for (j = 0; j < arr.length; ++j) {
-            if (mine[i] == arr[j]) {
-
-                totalmatches++;
-
-            }
-
-        }
+    for (var j = 0; j < eachRecipeIngredients.length; j++) {
+      eachIngredient = eachRecipeIngredients[j].name
+      // console.log(eachRecipeIngredients[j].name);
+      ingredientsArray.push(eachIngredient)
     }
+      console.log(ingredientsArray);
 
-    if (totalmatches/arr.length === 1) {
-        return 'Recipes match 100%';
-    } else if (totalmatches/arr.length >= 0.8) {
-        return "Recipes match greater than 80%";
-    } else if (totalmatches/arr.length >= 0.5) {
-        return "Recipes match greater than 50%";
-    } else {
-        return 'No good recipe matches';
-    }
-}
+    ///////// COMPARE MY INGREDIENTS WITH EACH RECIPE HERE ////////////////
+    check(ingredientsArray);
 
-//test arrays
-arr1 = ['fish','pasta','banana'] // 100%
-arr2 = ['fish','pasta','banana','clock','red salsa','duck'] //>=80%
-arr3 = ['fish','pasta','banana','clock'] //>=50%
-arr4 = ['car','phone','sauce','pasta'] //<=50%
-console.log(check(arr4));
+    ingredientsArray = [];
+    // console.log(ingredientsArray);
+
+} //end for-loop on allRecipes
+
+
+
+
+
+
+
+
+// //LOGIC ============================================================
+// mine = ['pasta','banana','fish','cheese','water','red salsa','duck','chicken']
+//
+// function check(arr) {
+//     var totalmatches = 0;
+//     for (i = 0; i < mine.length; i++) {
+//         for (j = 0; j < arr.length; ++j) {
+//             if (mine[i] == arr[j]) {
+//
+//                 totalmatches++;
+//
+//             }
+//
+//         }
+//     }
+//
+//     if (totalmatches/arr.length === 1) {
+//         return 'Recipes match 100%';
+//     } else if (totalmatches/arr.length >= 0.8) {
+//         return "Recipes match greater than 80%";
+//     } else if (totalmatches/arr.length >= 0.5) {
+//         return "Recipes match greater than 50%";
+//     } else {
+//         return 'No good recipe matches';
+//     }
+// }
+//
+// //test arrays
+// arr1 = ['fish','pasta','banana'] // 100%
+// arr2 = ['fish','pasta','banana','clock','red salsa','duck'] //>=80%
+// arr3 = ['fish','pasta','banana','clock'] //>=50%
+// arr4 = ['car','phone','sauce','pasta'] //<=50%
+// console.log(check(arr2));
